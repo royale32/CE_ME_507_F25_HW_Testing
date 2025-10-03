060cd60423b8b49eee00e6864857c2e7e124fde4
royale32 <epsroyales@gmail.com>
2025-10-03 14:25:03 -0600

    Add files via upload

--- HW7_MultiDimensionalMappings/MultiDimensionalMappings.py
+++ HW7_MultiDimensionalMappings/MultiDimensionalMappings.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 25 11:06:03 2024
+Created on Wed Sep 24 13:49:50 2025
 
-@author: kendrickshepherd
+@author: Owner
 """
 
 import sys
@@ -17,7 +16,8 @@ from matplotlib import pyplot as plt
 # a two-dimensional basis function at a point
 # Either copy that here or import the 
 # functionality using
-# import < INSERT YOUR MODULE NAME HERE >
+sys.path.insert(0, "../HW6_MultiDimensionalBasisFunctions/")
+import MultiDimensionalBasisFunctions as mdbf
 
 # This is a class that describes a Lagrange basis
 # in two dimensions
@@ -27,29 +27,45 @@ class LagrangeBasis2D:
     def __init__(self,degx,degy,interp_pts_x,interp_pts_y):
         self.degs = [degx,degy]
         self.interp_pts = [interp_pts_x,interp_pts_y]
+        self.n_bf = (self.degs[0]+1)*(self.degs[1]+1) # so I can refer to n_bf elsewhere in the code
         
     # the number of basis functions is the 
     # product of basis functions in the x (xi)
     # and y (eta) directions
     def NBasisFuncs(self):
-        return
+        n_bf = (self.degs[0]+1)*(self.degs[1]+1)
+        # (number of Lagrange basis functions in xi direction)*(number of basis functions in eta direction)
+        # (p+1)*(q+1)
+        return n_bf         # total number of 2D basis functions for basis
         
     # basis function evaluation code from 
     # previous homework assignment
     # this should be imported from that assignment
     # or copied before this class is defined
     def EvalBasisFunction(self,A,xi_vals):
-        return        
+        return mdbf.MultiDimensionalBasisFunction(A, self.degs, self.interp_pts, xi_vals)       
 
     # Evaluate a sum of basis functions times 
     # coefficients on the parent domain
     def EvaluateFunctionParentDomain(self, d_coeffs, xi_vals):
-        return
+        # self.NBasisFuncs()
+        uh = 0
+        for A in range(self.n_bf):
+            uh += d_coeffs[A]*self.EvalBasisFunction(A,xi_vals)
+        return uh
         
     # Evaluate the spatial mapping from xi and eta
     # into x and y coordinates
     def EvaluateSpatialMapping(self, x_pts, xi_vals):
-        return
+        x_y = []
+        x = 0
+        y = 0
+        for A in range(self.n_bf):
+            x += x_pts[A][0]*self.EvalBasisFunction(A,xi_vals)
+            y += x_pts[A][1]*self.EvalBasisFunction(A,xi_vals)
+        x_y.append(x)
+        x_y.append(y)
+        return x_y      # vector
     
     # Grid plotting functionality that is used
     # in all other plotting functions
@@ -170,5 +186,34 @@ class LagrangeBasis2D:
                 Z[i,j] = self.EvaluateFunctionParentDomain(d_coeffs,[xivals[i],etavals[j]])
     
         self.PlotGridData(X,Y,Z,contours=contours,zlabel=r"$u_h^e(x,y)$")
+        
 
-
+    
+def Problem5Plotting():
+    # class: when I make an instance of something, I'll put certain things in
+    degx = 2
+    degy = 2
+    x_pts = np.array([[0,0],[0,1],[1,1],[-1,0],[-1,2],[1,2],[-2,0],[-2,3],[1,3]])
+    interp_pts_x = np.linspace(-1,1,degx+1)
+    interp_pts_y = np.linspace(-1,1,degy+1)
+        
+    d_coeffs = [-1,2,3,5,6,7,2,1,3]
+        
+    Prob5Object = LagrangeBasis2D(degx, degy, interp_pts_x, interp_pts_y)
+    # Prob5Object.EvaluateFunctionParentDomain(d_coeffs, xi_vals)
+    
+        # Spatial to Parametric Mapping:
+    Prob5Object.PlotSpatialMapping(x_pts)
+    
+        # Solution field on the parent domain
+    Prob5Object.PlotParentSolutionField(d_coeffs)
+    
+        # 9 basis functions in both the parent and spatial domains
+    for i in range (9):             # loops through the A values; gives you a specific basis function for that A value
+        Prob5Object.PlotBasisFunctionParentDomain(i)
+        Prob5Object.PlotBasisFunctionSpatialDomain(i, x_pts)
+    
+        # Solution field on the spatial domain of the element's domain:
+    Prob5Object.PlotSpatialSolutionField(d_coeffs,x_pts)
+    
+Problem5Plotting()
