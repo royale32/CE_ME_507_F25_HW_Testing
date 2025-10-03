919f15823550e719b0cb23141a9b6ef46d0bdd94
royale32 <epsroyales@gmail.com>
2025-10-03 13:57:39 -0600

    Add files via upload

--- HW6_MultiDimensionalBasisFunctions/MultiDimensionalBasisFunctions.py
+++ HW6_MultiDimensionalBasisFunctions/MultiDimensionalBasisFunctions.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 10:18:05 2025
+
+@author: Owner
+"""
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -14,30 +21,66 @@ from matplotlib import pyplot as plt
 
 # IMPORT (or copy) your code from HW3 here
 # which evaluated a Lagrane polynomial basis function
-# import <UNCOMMENT THIS LINE AND INPUT YOUR FILENAME HERE>
+from CE_ME_507_Lagrange_Basis_Function_Code import LagrangeBasisEvaluation
+
+# idxs = a (a is a list of indexes)
+# degs = p (polynomial degrees in each direction)
+# interp_pts = set of interpolation points in each direction
+# xis = coordinates you want to evaluate
 
 # higher-dimensional basis function with multi-index
+# this function takes lists; these correspond to the arguments taken by the
+# LagrangeBasisEvaluation function from HW 3
+# LBE takes a basis function index (a)
+        # point (xi)
+        # set of interpolation points (pts)
+# MDBFI takes lists of each of the above
+# The expression we apply the product operator is N_{a_{i}}(xi_{i}); this is a single evaluation of the Lagrange basis
+# loop should take each a list item at each iteration
 def MultiDimensionalBasisFunctionIdxs(idxs,degs,interp_pts,xis):
-    return
+    Ni = 1              # initial value of basis function before loop
     
-# given an integer and a vector of basis function degrees extract
-# a corresponding multi-index out
-def ExtractMultiIndexFromSingleIndex(A,degs):
-    return
-
+    for i in range(len(idxs)):
+        p = degs[i]
+        pts = interp_pts[i]
+        xi = xis[i]
+        a = idxs[i]
+       
+        Ni *= LagrangeBasisEvaluation(p, pts, xi, a)
+    return Ni
+    
 # higher-dimensional basis function with single index
 def MultiDimensionalBasisFunction(A,degs,interp_pts,xis):
-    return
+    a_list = []
+    denom = degs[0]+1
+    ai = A % (degs[0]+1)
+    a_list.append(ai)
+    for i in range(1,len(degs)):
+        a_list.append(A//denom)
+        deg = degs[i]             # since p0 starts at i = 1
+        denom *= (deg+1)
+        """
+        if i == 0:
+            ai = A % (degs[0]+1)
+            a_list.append(ai)
+        else:
+            ai = A//denom
+            # print(A,ai)
+            a_list.append(ai)
+        """
+        # the code commented out didn't work; i changed it
+    result = MultiDimensionalBasisFunctionIdxs(a_list,degs,interp_pts,xis)
+    return result
     
 # plot of 2D basis functions with A a single index
-def PlotTwoDimensionalParentBasisFunction(A,degs,npts = 101,contours = True):
+def PlotTwoDimensionalParentBasisFunction(A,degs,npts = 101,contours = False):
     interp_pts = [np.linspace(-1,1,degs[i]+1) for i in range(0,len(degs))]
     xivals = np.linspace(-1,1,npts)
     etavals = np.linspace(-1,1,npts)
     
     Xi,Eta = np.meshgrid(xivals,etavals)
     Z = np.zeros(Xi.shape)
-    
+
     for i in range(0,len(xivals)):
         for j in range(0,len(etavals)):
             Z[i,j] = MultiDimensionalBasisFunction(A, degs, interp_pts, [xivals[i],etavals[j]])
@@ -59,4 +102,35 @@ def PlotTwoDimensionalParentBasisFunction(A,degs,npts = 101,contours = True):
         ax.set_ylabel(r"$\eta$")
         ax.set_zlabel(r"$N(\xi,\eta)$")
         plt.show()
-        
+
+def Input1():
+    # value for range = (p_0 + 1)*(p_1 + 1) = (1+1)*(1+1)=4
+    degs = [1,1]
+    for A in range(4):          # range 4 = 0,1,2,3 (total of 4)
+        PlotTwoDimensionalParentBasisFunction(A,degs)
+
+def Input2():
+    # value for range = (p_0 + 1)*(p_1 + 1) = (2+1)*(1+1)=6
+    degs = [2,1]
+    for A in range(6):
+        PlotTwoDimensionalParentBasisFunction(A,degs)
+
+def Input3():
+    # value for range = (p_0 + 1)*(p_1 + 1) = (2+1)*(2+1)=9
+    degs = [2,2]
+    for A in range(9):
+        PlotTwoDimensionalParentBasisFunction(A,degs)
+
+def Input4():
+    # value for range = (p_0 + 1)*(p_1 + 1) = (3+1)*(3+1)=16
+    degs = [3,3]
+    for A in range(16):
+        PlotTwoDimensionalParentBasisFunction(A,degs)
+    
+# Inputs for Problem 1:
+# Input1()
+# Input2()
+# Input3()
+# Input4()
+
+# change contour to false to see 3D
--- HW6_MultiDimensionalBasisFunctions/UniDimensionalXMap.py
+++ HW6_MultiDimensionalBasisFunctions/UniDimensionalXMap.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 23 21:18:02 2024
+Created on Mon Sep 22 10:02:39 2025
 
-@author: kendrickshepherd
+@author: Owner
 """
 
 import sys
@@ -14,12 +13,17 @@ from matplotlib import pyplot as plt
 
 # IMPORT (or copy) your code from HW3 here
 # which evaluated a Lagrane polynomial basis function
-# import <UNCOMMENT THIS LINE AND INPUT YOUR FILENAME HERE>
+sys.path.append('../HW3_Univariate_Lagrange/')
+import Univariate_Lagrange_Basis_Functions as ulbf
 
 # x(\xi) = \sum_{a=0}^p x_a * N_a(\xi)
 def XMap(deg,spatial_pts,interp_pts,xi):
     # complete this function
-    return
+    xe = 0
+    for a in range(0,deg+1):                              # goes to p (polynomial degree)
+        xe += spatial_pts[a]*ulbf.LagrangeBasisEvaluation(deg, interp_pts, xi, a)
+
+    return xe
 
 def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
     # parametric points to evaluate in Lagrange basis function
@@ -31,7 +35,6 @@ def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
         for i in range(0,len(xi_vals)):
             xi = xi_vals[i]
             xs.append(XMap(deg,spatial_pts,interp_pts,xi))
-        
         plt.plot(xi_vals,xs)
         plt.show()
 
@@ -51,3 +54,32 @@ def PlotXMap(deg,spatial_pts,interp_pts, npts=101, contours=True):
         ax.yaxis.set_ticklabels([])
         fig.colorbar(surf)
         plt.show()
+        
+def Input1():
+    deg = 1
+    spatial_pts = [0,1]
+    interp_pts = [-1,1]
+    PlotXMap(deg,spatial_pts,interp_pts)
+
+def Input2():
+    deg = 2
+    spatial_pts = [0.5,1,1.5]
+    interp_pts = [-1,0,1]
+    PlotXMap(deg,spatial_pts,interp_pts)
+    
+def Input3():
+    deg = 2
+    spatial_pts = [0.5,0.7,1.5]
+    interp_pts = [-1,-0.6,1]
+    PlotXMap(deg,spatial_pts,interp_pts)
+    
+def Input4():
+    deg = 2
+    spatial_pts = [0.5,0.7,1.5]
+    interp_pts = [-1,0,1]
+    PlotXMap(deg,spatial_pts,interp_pts)
+    
+Input1()
+Input2()
+Input3()
+Input4()
